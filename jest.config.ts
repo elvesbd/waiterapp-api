38a9db8fa1617fddb9b345b2e45538a07154af28
elvesbd <elvesbd@yahoo.com>
2023-09-15 18:42:54 -0300

    add connfig lintstaged

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import { Config } from 'jest';
+
+const config: Config = {
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '@products/(.+)': '<rootDir>/modules/products/$1',
+  },
+};
+
+export default config;
