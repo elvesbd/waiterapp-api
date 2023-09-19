c9f90a67bbe62b3d73c59f51e15841885d2738de
elvesbd <elvesbd@yahoo.com>
2023-09-19 17:57:49 -0300

    feat: implemented optimize image files

--- src/modules/shared/services/storage/supabase/file-storage.service.ts
+++ src/modules/shared/services/storage/supabase/file-storage.service.ts
@@ -1,8 +1,9 @@
-import { FileDto } from '@category/dto';
-import { FileStorageService } from '@category/infra';
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { FileStorageService } from '@category/infra';
+import { OptimizeImageFileService } from '@shared/utils';
 import { SupaBaseClientService } from '@shared/services/storage';
+import { FileDto } from '@category/dto';
 import {
   GetUrlStorageException,
   UploadFileStorageException,
@@ -11,8 +12,9 @@ import {
 @Injectable()
 export class SupaBaseFileStorageService implements FileStorageService {
   constructor(
-    private readonly supaBaseClientService: SupaBaseClientService,
     private readonly configService: ConfigService,
+    private readonly supaBaseClientService: SupaBaseClientService,
+    private readonly optimizeImageFileService: OptimizeImageFileService,
   ) {}
 
   private readonly logger = new Logger(SupaBaseClientService.name);
@@ -20,13 +22,16 @@ export class SupaBaseFileStorageService implements FileStorageService {
 
   async upload(file: FileDto): Promise<string> {
     const supabase = await this.getSupaBaseClient();
+    const optimizedFileBuffer = await this.optimizeImageFileService.handler(
+      file.buffer,
+    );
 
     try {
       const {
         data: { path },
       } = await supabase.storage
         .from(this.SUPABASE_BUCKET)
-        .upload(file.originalname, file.buffer, {
+        .upload(file.originalname, optimizedFileBuffer, {
           upsert: true,
         });
 
---
+++ src/modules/shared/utils/index.ts
@@ -0,0 +1,1 @@
+export { OptimizeImageFileService } from './optimize-image-file.service';
